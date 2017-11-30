f4e8bc7ae23a79ab4f6ebff0f01f4ea5b3d748c2
strota <32236607+strota@users.noreply.github.com>
2017-11-30 17:37:17 +0500

    п

--- async.js
+++ async.js
@@ -37,8 +37,7 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
 
         function finish(result, index) {
             results[index] = result;
-            countFinish += 1;
-            if (countFinish === jobs.length) {
+            if (results.length === jobs.length) {
                 resolve(results);
 
                 return true;
