ebd3cd5976e165753c6f1a4f2631c7388f553aff
strota <32236607+strota@users.noreply.github.com>
2017-11-30 05:33:51 +0500

    спустя долгое время

--- async.js
+++ async.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.isStar = true;
+exports.isStar = false;
 exports.runParallel = runParallel;
 
 /** Функция паралелльно запускает указанное число промисов
@@ -8,6 +8,83 @@ exports.runParallel = runParallel;
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
  */
+
+const queue = jobs.slice();
+const results = new Map();
+let runningCount = 0;
+return new Promise((resolve) => {
+    const result = [];
+    let finishedJobs = 0;
+    
+    +        let currentJob = 0;
+    +        const promises = makeArrOfPromises(jobs, timeout);
+    +        promises.slice(0, parallelNum)
+    +            .forEach((promise, idx) => start(promise, idx));
+    +
+    +        function start(promise) {
+    +            const idx = currentJob++;
+    +            const handler = data => finish(data, idx);
+    +            promise().then(handler);
+    +        }
+    +
+    +        function finish(data, idx) {
+    +            result[idx] = data;
+    +            finishedJobs++;
+    +            if (jobs.length === finishedJobs) {
+    +                resolve(result);
+    +            }
+    +            start(promises[currentJob]);
+    +        }
+    +    });
+     }
+
+
+
 function runParallel(jobs, parallelNum, timeout = 1000) {
     // асинхронная магия
+    return new Promise((resolve, reject) => {
+        job().then(resolve)
+        .catch(reject);
+
+    return new Promise((resolve) => {
+        const queue = jobs.slice();
+        const results = new Map();
+        let runningCount = 0;
+        
+        function start(job) {
+            runningCount++;
+
+            Promise.race([job().catch(err => err),
+                delay(timeout).then(() => new Error('Promise timeout'))])
+                .then(result => {
+                    results[job] = result;
+                    runningCount--;
+                    startNext();
+                    if (runningCount === 0) {
+                        onComplete();
+                    }
+                });
+        }
+        
+        function startNext() {
+            const job = queue.shift();
+            if (!job) {
+                return;
+            }
+            start(job);
+        }
+        function onComplete() {
+            resolve(jobs.map(job => results[job]));
+        }
+
+        for (let i = 0; i < parallelNum; i++) {
+            startNext();
+        }
+    });
+}
+
+function delay(timeout) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeout);
+    });
 }
